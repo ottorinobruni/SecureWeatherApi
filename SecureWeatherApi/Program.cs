d351f803e7213a8a1f21f628365a8976c21ab5d3
Ottorino Bruni <ottorino.bruni@gmail.com>
2024-12-17 15:38:57 +0100

    add refresh

--- SecureWeatherApi/Program.cs
+++ SecureWeatherApi/Program.cs
@@ -5,7 +5,21 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 
+// Key: RefreshToken, Value: Username
+var refreshTokens = new Dictionary<string, string>();
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add CORS Configuration
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
 // Bind JWT settings from configuration
 var jwtSettings = builder.Configuration
@@ -36,6 +50,11 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+
+// Enable CORS
+app.UseCors("AllowAll");
+
 app.UseAuthentication();
 app.UseAuthorization();
 
@@ -53,11 +72,26 @@ app.MapPost("/login", (UserCredentials credentials) =>
     if (credentials.Username == "username" && credentials.Password == "password")
     {
         var token = GenerateAccessToken(credentials.Username, jwtSettings);
-        return Results.Ok(new { AccessToken = token });
+        var refreshToken = Guid.NewGuid().ToString();
+        refreshTokens[refreshToken] = credentials.Username; 
+
+        return Results.Ok(new { AccessToken = token, RefreshToken = refreshToken });
     }
     return Results.Unauthorized();
 })
 .WithName("Login")
+.WithOpenApi();
+
+app.MapPost("/refresh", (RefreshRequest request) => {
+    if (refreshTokens.TryGetValue(request.RefreshToken, out var username))
+    {
+        var newAccessToken = GenerateAccessToken(username, jwtSettings);
+        return Results.Ok(new { AccessToken = newAccessToken });
+    }
+
+    return Results.BadRequest("Invalid refresh token");
+})
+.WithName("Refresh")
 .WithOpenApi();
 
 var summaries = new[]
@@ -112,6 +146,7 @@ string GenerateAccessToken(string username, JwtSettings jwtSettings)
 
 //Models
 record UserCredentials(string Username, string Password);
+record RefreshRequest(string RefreshToken);
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
